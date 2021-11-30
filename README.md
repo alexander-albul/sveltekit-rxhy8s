7c31d7ebf87c995bdf45781a489fc208ee156899
alexander-albul <ololouololou@gmail.com>
2021-12-01 02:23:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sveltekit-rxhy8s
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltekit-rxhy8s)
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+  let count = 0;
+</script>
+
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={ () => count += 1}>{count}</button>
